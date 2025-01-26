f8595bc8f4991323772ef12596ec8924c19effc4
Mahseeba Tanzeem <mahseebatanzeem777@gmail.com>
2025-01-26 19:11:08 +0530

    adding files

--- DIVANDCONQ/divandconq.java
+++ DIVANDCONQ/divandconq.java
@@ -49,9 +49,82 @@ public class divandconq{
             arr[i] = temp[k];
         }
     }
+
+    //quick sort
+    public static void quickSort(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+        //last element
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx-1); //left part
+        quickSort(arr, pIdx+1, ei); //right part
+    }
+
+    public static int partition(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i = si-1; //to make space for elements smaller than pivot
+
+        for(int j=si; j<ei; j++){
+            if(arr[j] <= pivot){
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
+    //Sorted and rotated array search
+    public static int search(int arr[], int tar, int si, int ei) {
+        if(si > ei){
+            return -1;
+        }
+
+        int mid = si+(ei-si)/2;
+
+        //case found
+        if(arr[mid] == tar) {
+            return mid;
+        }
+
+        //mid on L1
+        if(arr[si] <= arr[mid]){
+            //case a: left
+            if(arr[si] <= tar && tar <= arr[mid]) {
+                return search(arr, tar, si, mid-1);
+            } else {
+                //case b: right
+                return search(arr, tar, mid+1, ei);
+            }
+        }
+        //mid on L2
+        else {
+            //case c: right
+            if(arr[mid] <= tar && tar <= arr[ei]) {
+                return search(arr, tar, mid+1, ei);
+            } else {
+                //case d: left
+                return search(arr, tar, si, mid-1);
+            }
+        }
+    }
+
     public static void main(String args[]){
-        int arr[] = {6,3,9,5,2,8};
-        mergeSort(arr, 0, arr.length-1);
-        printArr(arr);
+        // int arr[] = {6,3,9,5,2,8};
+        // mergeSort(arr, 0, arr.length-1);
+        // quickSort(arr, 0, arr.length-1);
+        // printArr(arr);
+        int arr[] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 6;
+        int tarIdx = search(arr, target, 0, arr.length-1);
+        System.out.println(tarIdx);
+        
     }
 }
