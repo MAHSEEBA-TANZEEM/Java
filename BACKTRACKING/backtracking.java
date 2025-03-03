4cea671f31832570de63f55cfe0501ffe30a7e7a
Mahseeba Tanzeem <mahseebatanzeem777@gmail.com>
2025-03-03 07:24:05 +0530

    add files

--- BACKTRACKING/backtracking.java
+++ BACKTRACKING/backtracking.java
@@ -100,21 +100,73 @@ public class backtracking {
     //     return true;
     // }
     // nQueens -count ways
-    public static void nQueens(char board[][], int row){
+    // public static void nQueens(char board[][], int row){
+    //     // base case
+    //     if(row == board.length){
+    //         // printBoard(board);
+    //         count++;
+    //         return;
+    //     }
+    //     // column loop
+    //     for(int j=0; j<board.length; j++){
+    //         if(isSafe(board, row, j)){
+    //             board[row][j] = 'Q';
+    //             nQueens(board, row+1);
+    //             board[row][j] = 'x';
+    //         }
+    //     }
+    // }
+    // public static void printBoard(char board[][]) {
+    //     System.out.println("-------chess board-------");
+    //     for(int i=0; i<board.length; i++){
+    //         for(int j=0; j<board.length; j++){
+    //             System.out.print(board[i][j]+ " ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+    // public static boolean isSafe(char board[][], int row, int col){
+    //     // vertically up 
+    //     for(int i=row-1; i>=0; i--){
+    //         if(board[i][col] == 'Q'){
+    //             return false;
+    //         }
+    //     }
+    //     // diagnol left up
+    //     for(int i=row-1, j=col-1; i>=0 && j>=0; i--,j--){
+    //         if(board[i][j] == 'Q'){
+    //             return false;
+    //         }
+    //     }
+    //     // diagnol right up
+    //     for(int i=row-1, j=col+1; i>=0 && j<board.length; i--,j++){
+    //         if(board[i][j] == 'Q'){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    static int count = 0;
+    
+    // nQueens-1soluton
+    public static boolean nQueens(char board[][], int row){
         // base case
         if(row == board.length){
             // printBoard(board);
             count++;
-            return;
+            return true;
         }
         // column loop
         for(int j=0; j<board.length; j++){
             if(isSafe(board, row, j)){
                 board[row][j] = 'Q';
-                nQueens(board, row+1);
+                if(nQueens(board, row+1)){
+                    return true;
+                }
                 board[row][j] = 'x';
             }
         }
+        return false;
     }
     public static void printBoard(char board[][]) {
         System.out.println("-------chess board-------");
@@ -146,8 +198,7 @@ public class backtracking {
         }
         return true;
     }
-    static int count = 0;
-    public static void main(String args[]) {
+        public static void main(String args[]) {
         // int arr[] = new int[5];
         // changeArr(arr, 0, 1);
         // printArr(arr);
@@ -155,7 +206,17 @@ public class backtracking {
         // findSubsets(str, "", 0);
         // String str = "abc";
         // findPermutations(str, "");
-        int n=2;
+        // int n=2;
+        // char board[][] = new char[n][n];
+        // // initialize
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<n; j++){
+        //         board[i][j] = 'x';
+        //     }
+        // }
+        // nQueens(board, 0);
+        // System.out.println("total ways to solve n queens =" + count);
+        int n=4;
         char board[][] = new char[n][n];
         // initialize
         for(int i=0; i<n; i++) {
@@ -163,7 +224,13 @@ public class backtracking {
                 board[i][j] = 'x';
             }
         }
-        nQueens(board, 0);
-        System.out.println("total ways to solve n queens =" + count);
+        if(nQueens(board, 0)){
+           System.out.println("solution is possible"); 
+           printBoard(board);
+        } else {
+            System.out.println("solution is not possible"); 
+        }
     }
 }
+
+
