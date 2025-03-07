b5eb780dbb79ac46dbbf89754d7dc8e007cefd50
Mahseeba Tanzeem <mahseebatanzeem777@gmail.com>
2025-03-07 20:02:57 +0530

    add files

--- ARRAYLIST/Arraylist.java
+++ ARRAYLIST/Arraylist.java
@@ -96,39 +96,113 @@ public class Arraylist {
         // System.out.println(mainList);
 
         // multi dimensional eg2
-        ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+        // ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
 
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-        ArrayList<Integer> list3 = new ArrayList<>();
+        // ArrayList<Integer> list1 = new ArrayList<>();
+        // ArrayList<Integer> list2 = new ArrayList<>();
+        // ArrayList<Integer> list3 = new ArrayList<>();
 
-        for(int i=1; i<=5; i++){
-            list1.add(i*1);
-            list2.add(i*2);
-            list3.add(i*3);
-        }
-        mainList.add(list1);
-        mainList.add(list2);
-        mainList.add(list3);
+        // for(int i=1; i<=5; i++){
+        //     list1.add(i*1);
+        //     list2.add(i*2);
+        //     list3.add(i*3);
+        // }
+        // mainList.add(list1);
+        // mainList.add(list2);
+        // mainList.add(list3);
 
-        System.out.println(mainList);
+        // System.out.println(mainList);
 
     
-        for(int i=0; i<mainList.size(); i++){
-            ArrayList<Integer> currList = mainList.get(i);
-            for(int j=0; j<currList.size(); j++){
-                System.out.print(currList.get(j)+" ");
-            }
-            System.out.println();
-        }
-        System.out.println(mainList);
+        // for(int i=0; i<mainList.size(); i++){
+        //     ArrayList<Integer> currList = mainList.get(i);
+        //     for(int j=0; j<currList.size(); j++){
+        //         System.out.print(currList.get(j)+" ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println(mainList);
 
+        // CONTAINER WITH MOST WATER -BRUTE FORCE and 2 POINTER APPROACH
+        // ArrayList<Integer> height = new ArrayList<>();
+        // height.add(1);
+        // height.add(8);
+        // height.add(6);
+        // height.add(2);
+        // height.add(5);
+        // height.add(4);
+        // height.add(8);
+        // height.add(3);
+        // height.add(7);
 
+        // System.out.println(storeWater(height));
+
+        // pair sum1- brute force
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 5;
+        
+        System.out.println(pairSum1(list, target));
     }
+
     // public static void swap(ArrayList<Integer>list, int idx1, int idx2){
     //     int temp = list.get(idx1);
     //     list.set(idx1, list.get(idx2));
     //     list.set(idx2, temp);
     // }
+
+    // public static int storeWater(ArrayList<Integer> height){
+    //     int maxWater = 0;
+    //     // brute force- O(n^2)
+    //     for(int i=0; i<height.size(); i++){
+    //         for(int j=i+1; j<height.size(); j++){
+    //             int ht = Math.min(height.get(i), height.get(j));
+    //             int width = j-i;
+    //             int currWater = ht * width;
+    //             maxWater = Math.max(maxWater, currWater);
+    //         }
+    //     }
+    //     return maxWater;
+    // }
+
+    // 2 pointer approach-O(n)
+    // public static int storeWater(ArrayList<Integer> height){
+    //     int maxWater = 0;
+    //     int lp = 0;
+    //     int rp = height.size()-1;
+
+    //     while(lp < rp){
+    //         //calculate water area
+    //         int ht = Math.min(height.get(lp), height.get(rp));
+    //         int width = rp-lp;
+    //         int currWater = ht * width;
+    //         maxWater = Math.max(maxWater, currWater);
+
+    //         //update ptr
+    //         if(height.get(lp) < height.get(rp)){
+    //             lp++;
+    //         } else {
+    //             rp--;
+    //         }
+    //     }
+    //     return maxWater;
+    // }
+
+    // pair sum brute-force
+    public static boolean pairSum1(ArrayList<Integer> list, int target){
+         for(int i=0; i<list.size(); i++){
+            for(int j=i+1; j<list.size(); j++){
+                if(list.get(i) + list.get(j) == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     
 }
